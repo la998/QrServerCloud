f4ee7b61a3c6cff4ef747b043648b471fff84e9b
Leaver <mrluchanglong@163.com>
2025-04-15 22:50:33 +0800

    mysql nacos 用户权限设置

--- env/configs/mysql/0-init.sql
+++ env/configs/mysql/0-init.sql
@@ -22,9 +22,11 @@ GRANT ALL PRIVILEGES ON seata.* TO 'seata'@'%';
 
 -- auth-service 认证模块 数据库
 CREATE DATABASE IF NOT EXISTS auth_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT SELECT, INSERT, UPDATE, DELETE ON auth_db.* TO 'nacos'@'%';
+GRANT ALL PRIVILEGES ON auth_db.* TO 'nacos'@'%' WITH GRANT OPTION;
+
 
 -- user-service 用户模块 数据库
 CREATE DATABASE IF NOT EXISTS user_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL PRIVILEGES ON auth_db.* TO 'nacos'@'%' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
