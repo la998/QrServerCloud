d23e2a314903e399f08ddb5af2dc14a02502d52d
Leaver <mrluchanglong@163.com>
2025-04-18 22:47:03 +0800

    env基础设施分离到独立项目

--- user-service/src/main/kotlin/com/qr/user/UserApplication.kt
+++ user-service/src/main/kotlin/com/qr/user/UserApplication.kt
@@ -1,13 +1,1 @@
-package com.qr.user
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-
-@EnableDiscoveryClient
-@SpringBootApplication
-class UserApplication
-
-fun main(args: Array<String>) {
-    runApplication<UserApplication>(*args)
-}
+package com.qr.userimport org.springframework.boot.autoconfigure.SpringBootApplicationimport org.springframework.boot.runApplicationimport org.springframework.cloud.client.discovery.EnableDiscoveryClient@EnableDiscoveryClient@SpringBootApplicationclass UserApplicationfun main(args: Array<String>) {    runApplication<UserApplication>(*args)}
